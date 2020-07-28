ef49e14e57620afec1f63acb5955cb07f979a918
Kadugs <cadu20.gomes@hotmail.com>
2020-07-28 19:45:34 -0300

    commit aula 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+//desafio da descrição
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+    <Route path="/cadastro/video" component={CadastroVideo}/>
+    <Route path="/cadastro/Categoria" component={CadastroCategoria}/>
+    <Route path="/" component={Home} exact/>
+    <Route component={Pagina404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
